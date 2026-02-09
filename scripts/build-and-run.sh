f392196e9c702ffa1688f1bb6fba9e309a429c7d
Paulo F. Oliveira <paulo.oliveira@kivra.com>
2026-02-09 15:12:40 +0000

    Prevent priv-specific weirdness
    This is a test script, so kept locally
    We don't want to have to deal with weirdness around
    priv being shipped with the software solution

---
+++ scripts/build-and-run.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+# Expected input (auto-populated when in GitHub Actions)
+: "${GITHUB_WORKSPACE:=}"
+: "${GITHUB_API_URL:=}"
+: "${GITHUB_SERVER_URL:=}"
+: "${GITHUB_WORKFLOW:=}"
+: "${GITHUB_JOB:=}"
+: "${GITHUB_REF:=}"
+: "${GITHUB_REPOSITORY:=}"
+: "${GITHUB_RUN_ID:=}"
+: "${GITHUB_SHA:=}"
+: "${GITHUB_TOKEN:=}"
+: "${GITHUB_WORKSPACE:=}"
+
+echo "Building... this may take a while..."
+docker build --build-arg "workdir=${PWD}" . || exit $?
+echo "Done!"
+
+echo "Running... this may take a while..."
+docker run \
+    --volume ".:${GITHUB_WORKSPACE}" \
+    --workdir "${GITHUB_WORKSPACE}" \
+    --env GITHUB_API_URL="${GITHUB_API_URL}" \
+    --env GITHUB_SERVER_URL="${GITHUB_SERVER_URL}" \
+    --env GITHUB_WORKFLOW="${GITHUB_WORKFLOW}" \
+    --env GITHUB_JOB="${GITHUB_JOB}" \
+    --env GITHUB_REF="${GITHUB_REF}" \
+    --env GITHUB_REPOSITORY="${GITHUB_REPOSITORY}" \
+    --env GITHUB_RUN_ID="${GITHUB_RUN_ID}" \
+    --env GITHUB_SHA="${GITHUB_SHA}" \
+    --env GITHUB_TOKEN="${GITHUB_TOKEN}" \
+    --env GITHUB_WORKSPACE="${GITHUB_WORKSPACE}" \
+    "$(docker build -qq .)" \
+    $@
+echo "Done!"
